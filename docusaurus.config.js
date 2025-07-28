cea558ceb95e677f27067f10482b68da9344f5df
crzliang <lkzxgp@gmail.com>
2025-07-28 22:29:26 +0800

    更新编辑链接至新的 GitHub 仓库路径

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/GUETSEC/guetsec',
+            'https://github.com/GUETSEC/guetsec/tree/main/',
         },
         blog: false,
         theme: {
